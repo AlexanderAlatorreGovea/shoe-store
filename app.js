c9c69d1ba43a0a6e314aec6c4a1da89b11cfeba7
Alexander Govea <alexander_alatorre1993@hotmail.com>
2020-03-09 19:26:18 -0700

    v.1.1

--- app.js
+++ app.js
@@ -37,10 +37,10 @@ if (process.env.NODE_ENV = 'development') {
 }
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, '/client/build')));
 
   app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
   });
 }
 
