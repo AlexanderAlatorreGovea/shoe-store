6c16453dd3495fe8f126c5677b2cdbf64bcf18e7
Alexander Govea <alexander_alatorre1993@hotmail.com>
2020-03-09 17:43:40 -0700

    added file

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const DB = process.env.DATABASE.replace(
 );
 
 mongoose
-  .connect('mongodb+srv://alexandergovea19:Freshman123.@cluster0-yyfkd.mongodb.net/test?retryWrites=true&w=majority', {
+  .connect('mongodb+srv://alexandergovea19:Freshman123.@cluster0-yyfkd.mongodb.net', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
