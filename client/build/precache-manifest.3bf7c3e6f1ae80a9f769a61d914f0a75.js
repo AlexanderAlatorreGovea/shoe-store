114358dc601c625da97e6d30bc2dd040dd4954e2
Alexander Govea <alexander_alatorre1993@hotmail.com>
2020-03-12 19:59:43 -0700

    v.1.1

---
+++ client/build/precache-manifest.3bf7c3e6f1ae80a9f769a61d914f0a75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "867041cca53942e17f0c5073289d8477",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bef87a6ef142bbee4517",
+    "url": "/static/css/main.fa6e339f.chunk.css"
+  },
+  {
+    "revision": "2a0006fbf1b12603b799",
+    "url": "/static/js/2.33bb4117.chunk.js"
+  },
+  {
+    "revision": "bef87a6ef142bbee4517",
+    "url": "/static/js/main.8a05ed10.chunk.js"
+  },
+  {
+    "revision": "8de9e59d7e3b1acbe7d5",
+    "url": "/static/js/runtime~main.4f3d2d87.js"
+  },
+  {
+    "revision": "edaae9ae94204dd0e95f5001aa1a7cf1",
+    "url": "/static/media/adidas-yeezy-boost-350-white.edaae9ae.png"
+  }
+]);
