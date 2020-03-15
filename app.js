4eb7f3a70787251ea401bbe0c144209651e06397
Alexander Govea <alexander_alatorre1993@hotmail.com>
2020-03-14 19:58:12 -0700

    v.1.1.0

--- app.js
+++ app.js
@@ -30,10 +30,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Set security HTTP headers
 app.use(helmet());
-
-if (process.env.NODE_ENV = 'development') {
-  app.use(morgan('dev'));
-}
+ 
+// if (process.env.NODE_ENV = 'development') {
+  // app.use(morgan('dev'));
+// }
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')), function (err) {
