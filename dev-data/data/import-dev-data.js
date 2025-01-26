26e9bb7d80b8bba84cf2fefe894cd1f11c819469
Alexander <Alexander_alatorre1993@hotmail.com>
2025-01-26 14:38:23 -0800

    removed commented out code

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,39 +1,35 @@
-const fs = require('fs');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const Product = require('../../models/productsModel');
-const User = require('../../models/userModel');
+const fs = require("fs");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const Product = require("../../models/productsModel");
+const User = require("../../models/userModel");
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: "./config.env" });
 
 const DB = process.env.DATABASE;
-// .replace(
-//   '<PASSWORD>',
-//   process.env.DATABASE_PASSWORD
-// );
 
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
   })
-  .then(() => console.log('DB connection successful!'));
+  .then(() => console.log("DB connection successful!"));
 
 //Read Json FILE_EXTNAME
 
- const products = JSON.parse(
-   fs.readFileSync(`${__dirname}/shoe-data.json`, 'utf-8')
- );
+const products = JSON.parse(
+  fs.readFileSync(`${__dirname}/shoe-data.json`, "utf-8")
+);
 
-const users = JSON.parse(fs.readFileSync(`${__dirname}/clients.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/clients.json`, "utf-8"));
 
 //IMPORT DATA INTO DB
 const importData = async () => {
   try {
     await Product.create(products);
     await User.create(users, { validateBeforeSave: false });
-    console.log('Data successfully loaded!');
+    console.log("Data successfully loaded!");
   } catch (err) {
     console.log(err);
   }
@@ -45,21 +41,21 @@ const deleteData = async () => {
   try {
     await Product.deleteMany();
     await User.deleteMany();
-    console.log('Data successfully deleted!');
+    console.log("Data successfully deleted!");
   } catch (err) {
     console.log(err);
   }
   process.exit();
 };
 
-if (process.argv[2] === '--import') {
+if (process.argv[2] === "--import") {
   importData();
-} else if (process.argv[2] === '--delete') {
+} else if (process.argv[2] === "--delete") {
   deleteData();
 }
 
 console.log(process.argv);
 
 //to use in the terminal to modify the data
-    //node dev-data/data/import-dev-data.js  --delete
-    //node dev-data/data/import-dev-data.js  --import
+//node dev-data/data/import-dev-data.js  --delete
+//node dev-data/data/import-dev-data.js  --import
